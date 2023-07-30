db37b946b69f22279bada0ecda56bfce40a86ab0
Adi Levi <adile0194@gmail.com>
2023-07-30 16:04:59 +0300

    switch

--- js/script.js
+++ js/script.js
@@ -10,26 +10,37 @@ $(document).ready(function () {
             let query = $("#querSel").val()
             let sel = $("#querSel");
             sel.css("display", "none");
-            let ttlChoice = $("ttlChoice");
 
             if ((query == 1) || (query = 5) || (query = 6) || (query = 7) || (query = 8)) {
+                let ttlChoice = $("ttlChoice");
                 ttlChoice.html = "<h3>Enter Inputs for choice " + query + "</h3";
+                ttlChoice.css("display, block");
                 switch (query) {
-                    case 1:
+                    case 1: {
                         let pastEv = $("#pastEv");
                         pastEv.css("display", "block");
-                    case 5:
+                        break;
+                    }
+                    case 5: {
                         let incomes = $("#incomes");
                         incomes.css("display", "block");
-                    case 6:
+                        break;
+                    }
+                    case 6: {
                         let insertEmp = $("#insertEmp");
                         insertEmp.css("display", "block");
-                    case 7:
+                        break;
+                    }
+                    case 7: {
                         let discount = $("#discount");
                         discount.css("display", "block");
-                    case 8:
+                        break;
+                    }
+                    case 8: {
                         let saleIncome = $("#saleIncome");
                         saleIncome.css("display", "block");
+                        break;
+                    }
                 }
                 count++;
             } else {
